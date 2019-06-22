80b3a0d465a7955112051071cba8a9b41aad1d1d
Eugene Shishlannikov <eg343462@dal.ca>
2019-06-22 18:45:29 -0300

    refactoring

--- Employer/api/controllers/EmployerController.js
+++ Employer/api/controllers/EmployerController.js
@@ -63,8 +63,8 @@ module.exports = {
        }
       })
     },
+
     supplyMBRinfo:function(req, res) {
-      // console.log("sdfsdfsdfsdfsd");
       var employeeId = req.body.empID;
       console.log("sdfsdfsdfsdfsd=>"+ employeeId);
 
@@ -84,54 +84,27 @@ module.exports = {
         var endpointURL = "https://cloud-mortgage-web-service.herokuapp.com/verify?name="+name+"&email="+email+"&id="+mbrID+"&tenure="+tenure+"&salary="+salary+"";
         console.log("URL -> "+endpointURL);
         res.redirect(endpointURL);
-
-        // if(0 == result.length) {
-        //   res.send("Failed to authenticate the employee with the ID " + employeeId);
-        // }
-        //   if(data.password === password){
-        //      res.redirect("https://cloud-mortgage-web-service.herokuapp.com/verify?name=Nirav&email=niravs200@dal.ca&id=1&tenure=2&salary=35000");
-        //   }
-        //   else{
-        //    res.redirect("http://localhost:1337/employee/authenticate?authenticated=false");
-        // }
        })
-    },
-// ,
-//   'authenticate-and-send-info-to-broker': function (req, res) {
-//    // var mortgageApplicationNum =
-//    // var mortgageBrockerApiUrl = req.body.password;
-//    var password=req.body.password;
-//    console.log(password);
-//     var employeeId = req.body.empID;
+    },   
 
-//     Employer.find({empID: employeeId}).exec(function(err, result) {
-//       if (err) {
-//         res.send(500, { error: "Database Error when retrieving info about employee with ID " + employeeId});
-//       }
+    authenticateUser: function (req, res) {
+      var password=req.body.password;
+      var employeeId = req.body.empID;
+      Employer.find({empID: employeeId}).exec(function(err, result) {
+        var data = result[0];
+        if (err) {
+          res.send(500, { error: "Database Error when retrieving info about employee with ID " + employeeId});
+        }
 
-//       if(0 == result.length) {
-//         res.send("Failed to authenticate the employee with the ID " + employeeId);
-//       }
-//       else{
-//         res.redirect("http://localhost:1337/supply");
-//       }
-//       result[0].applicationNumber = mortgageApplicationNum;
-
-//       request.get({
-//         type: "POST",
-//         // url: mortgageBrockerApiUrl,
-//         url: 'https://b00806895-cloud-a3-backend.herokuapp.com/students/students-and-books895',
-//         data: JSON.stringify(result[0]),
-//         contentType: "application/json; charset=utf-8",
-//         dataType: "json"},
-//         function(error, response, body) {
-//           if (error) {
-//             console.log(error);
-//           }
-//           else {
-//             res.send(response);
-//           }
-//       })
-//     });
-//   }
+        if(0 == result.length) {
+          res.send("Failed to authenticate the employee with the ID " + employeeId);
+        }
+          if(data.password === password){
+            res.redirect("https://company-portal-frontend.herokuapp.com/employee/authenticate?authenticated=true&empID="+employeeId);
+          }
+          else{
+            res.redirect("https://company-portal-frontend.herokuapp.com/employee/authenticate?authenticated=false");
+          }
+      })
+    },  
 };
